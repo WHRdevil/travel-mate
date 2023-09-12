93e24995693fe6005e6bb099d182fa4020993d9d
lonzzi <lonzzi@qq.com>
2023-09-12 10:00:07 +0800

    change route

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,5 +1,25 @@
-<script setup></script>
+<script setup>
+const images = [
+  'https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg',
+  'https://fastly.jsdelivr.net/npm/@vant/assets/apple-2.jpeg'
+]
+</script>
 
-<template></template>
+<template>
+  <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+    <van-swipe-item>1</van-swipe-item>
+    <van-swipe-item>2</van-swipe-item>
+    <van-swipe-item>3</van-swipe-item>
+    <van-swipe-item>4</van-swipe-item>
+  </van-swipe>
+</template>
 
-<style scoped></style>
+<style scoped>
+.my-swipe .van-swipe-item {
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  text-align: center;
+  background-color: #39a9ed;
+}
+</style>
